824200e74797d7f60308cab82f1eb10fc0c158ac
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-07 15:51:45 -0700

    Update question2.js

--- practicum/week-1/question2.js
+++ practicum/week-1/question2.js
@@ -14,7 +14,7 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
      for(i = 0; i < cookies.length; i++){
          numCookies += cookies[i];
      }
-     if((numCookies / extraCookies) == 0){
+     if((numCookies % extraCookies) == 0){
          return true;
      }
      return false;
