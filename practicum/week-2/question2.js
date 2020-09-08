9b510e7605670fc9a66f07ad32175ca5a7dfd0ce
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-07 20:13:36 -0700

    Update question2.js

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -10,6 +10,23 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
+     let start = 0; 
+     let end = 0;
+     let max = 0;
+     let ending = 0;
+     let beginning = 0;
+     for (i = 0; i < input.length; i++){
+       ending = ending + input[i];
+      if (ending < 0){
+       ending = 0;
+       beginning = i + 1;
+      }
+      if (max < ending){
+       max = ending;
+       start = beginning;
+       end = i;
+      }
+     }
+  return max;
     
  };
