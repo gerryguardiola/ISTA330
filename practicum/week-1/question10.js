d3a18f44e8a7f98b61890a03db8e3d5d4d983010
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-07 19:18:38 -0700

    Update question10.js

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -8,23 +8,5 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
- let s = n.toString();
- let digitArray = [];
- let max = 0; 
- let min = 10;
- 
- for(i = 0; i < s.length; i++){
-  digitArray.push(parseInt(s[i])));
- }
- 
- for(j = 0; j < digitArray.length; j++){
-  if (digitalArray[j] > max){
-   max = digitalArray[j];
-  }
-  if (digitalArray[j] < min){
-   min = digitalArray[j];
-  }
- }
- let val = max - min;
- return val;
+ return 1;
 };
