f46830b3e45c6400a9c47351924e1761175dfe08
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-07 17:50:41 -0700

    Update question9.js

--- practicum/week-1/question9.js
+++ practicum/week-1/question9.js
@@ -9,5 +9,14 @@ output: 'hello'
 */
 
 var suffleString = function(input, shuffleIndices) {
-
+var string = "";
+  for (i = 0; i < shuffleIndices.length; i++){
+    if (i < shuffleIndices.length-1){
+       string += input.substring(shuffleIndices[i], shuffleIndices[i+1]);
+    }
+    else{
+       string += input.substring(shuffleIndices[i]);
+    }
+  }
+  return string
 };
