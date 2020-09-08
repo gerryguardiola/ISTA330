49d10280f26b223485889f6eda55d8afa8bbd293
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-07 21:44:50 -0700

    Update question7.js

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -12,16 +12,5 @@ output: [[1,4], [2,5], [3,6]]
 */
 
 var transpose = function(M) {
-let rows = input[0].length;
-let cols = input.length;
-let array = [];
-for (k = 0; k < rows; k++){
-  array[k] = [];
-}
-  for (i = 0; i < cols; i++){
-    for (j = 0; j < rows; j++){
-      array[j].push(input[i][j]);
-    }
-  }
-  return array;
+return 1;
 };
