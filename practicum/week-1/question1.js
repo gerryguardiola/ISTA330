c33bda371f59be22b095c7d1eb1c8f599b061d8d
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-07 18:40:05 -0700

    Update question1.js

--- practicum/week-1/question1.js
+++ practicum/week-1/question1.js
@@ -6,8 +6,8 @@ For example given the input array [5, 6, 1], the running sum is
 */
 
 var runningSum = function(input) {
-   var sumArray = [];
-   var sum = 0;
+   let sumArray = [];
+   let sum = 0;
    
    for (i = 0; i < input.length; i++){
      sum += input[i]
