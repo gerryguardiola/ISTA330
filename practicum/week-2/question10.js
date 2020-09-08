8db3844b00a9770a4bb3da18e5011ce5b1fca139
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-07 22:29:45 -0700

    Update question10.js

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -16,26 +16,5 @@ output: [[-5, -4], [23, 24]]
 */
 
 var minPairs = function(input) {
-let diff = 10000;
-let array = [];
-let input = input.sort();
-for(i = 0; i < input.length-1; i++){
- if (input[i+1] < input[i]){
-  continue;
- }
- let min = input[i+1] - input[i];
- if (min == diff){
-  array[array.length] = [];
-  array[array.length].push(input[i]);
-  array[array.length].push(input[i+1]);
- }
- if(min < diff){
-  let array = [];
-  array[0] = [];
-  array[0].push(input[i]);
-  array[0].push(input[i+1])
-  diff = min;
- }
-}
-return array;
+
 };
