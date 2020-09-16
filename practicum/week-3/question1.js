3aa84f8574439fe3fd2c9ee54e601a9a1139a77e
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-16 15:57:12 -0700

    Update question1.js

--- practicum/week-3/question1.js
+++ practicum/week-3/question1.js
@@ -20,5 +20,19 @@ Return the number of teams you can form given the conditions.
 */
 
 var howManyTeams = function(input) {
-   
+   let num = 0;
+   let len = input.length;
+   for (let i = 0; i + 2 < len; i++) {
+       for (let j = i + 1; j + 1 < len; j++) {
+           for (let k = j + 1; k < len; k++) {
+               if (input[i] > input[j] && input[j] > input[k]){ 
+                 num++;
+               }
+               if (input[i] < input[j] && input[j] < input[k]){
+                 num++;
+               }
+           }
+       }
+    }
+    return num;
 };
