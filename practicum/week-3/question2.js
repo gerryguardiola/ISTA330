675d6e3104a37bccedf53e9ded772ceb514b0c49
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-16 16:05:39 -0700

    Update question2.js

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -11,6 +11,17 @@ Example: input: [19,3,2,10,8,2,3,5]
           */
 
  var findDuplicates = function(input) {
-     
-    
+    let inputSet = new Set(input);
+          
+    input.forEach(num => {
+        if (inputSet.has(num)) {
+            inputSet.delete(num);
+        } 
+        else {
+            inputSet.add(num);
+        }
+    });
+    let duplicateArr = Array.from(inputSet);  
+          
+    return duplicateArr;
  };
