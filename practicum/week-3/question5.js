fe5ac800e2f71c375d3b5c7760499084a23e2ee7
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-17 15:58:07 -0700

    Update question5.js

--- practicum/week-3/question5.js
+++ practicum/week-3/question5.js
@@ -10,6 +10,23 @@ output: [24,12,8,6]
 */
 
 var productOfOthers = function(input) {
-
+ let ans = [];
+ productLoop(input, ans, i = 0);
+ 
+ return ans;
 };
 
+var productLoop = (input, ans, i) => {
+ let curr = 1;
+  for(j = 0; j < input.length; j++){
+   for(k = 0; k < input.length; k++){
+      if (k != i){
+       curr = curr * input[k];
+      }
+   }
+   i++;
+   ans.push(curr);
+   curr = 1;
+  }
+ return;
+};
