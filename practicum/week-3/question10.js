4f49cccb7a892d6768a84c636613ce6a7fa2fcac
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-17 22:15:22 -0700

    Update question10.js

--- practicum/week-3/question10.js
+++ practicum/week-3/question10.js
@@ -13,8 +13,9 @@ output: true
 var isPalindrome = function(s) {   
   let i = 0; 
   let arr = s.split(" ");
-  let string = arr.join('');
+  let string = arr.join("");
   let j = string.length - 1;
+  
   while (i < j) {
    let firstChar = string[i].toLowerCase()
    let lastChar = string[j].toLowerCase()
