8f7423e69037fde7a2da45517e8f6aff454b0caf
gerryguardiola <70231755+gerryguardiola@users.noreply.github.com>
2020-09-17 21:54:21 -0700

    Update question10.js

--- practicum/week-3/question10.js
+++ practicum/week-3/question10.js
@@ -14,7 +14,7 @@ var isPalindrome = function(s) {
   if (s == ""){
     return true;
   }
-  s = s.toLowerCase();
+  let str = s.toLowerCase();
   let arr = s.split(" ");
   let string = arr.join("")
   let boolean = recursivePalindrome(string);
@@ -26,15 +26,15 @@ var recursivePalindrome = (string) => {
     return true;
   }
   if (string.chatAt(0) == '-' || string.chatAt(0) == '?' ||  string.chatAt(0) == ','){
-    recursivePalindrome(string.substring(1));
+    return recursivePalindrome(string.substring(1));
   }
   if (string.chatAt(string.length -1 ) == '-' || string.chatAt(string.length -1) == '?' 
       ||  string.chatAt(string.length -1) == ','){
-    recursivePalindrome(string.substring(0, string.length - 1));
+    return recursivePalindrome(string.substring(0, string.length - 1));
   }
   
   if (string.charAt(0) != string.chatAt(string.length - 1)){
     return false;
   }
-  recursivePalindrome(string.substring(1, string.length -1));
+  return recursivePalindrome(string.substring(1, string.length -1));
 };
